e7753444e1178f68b1c461b0b4fc8f29c5db95ed
CPP-Bot-78 <kzachoulitis@gmail.com>
2023-05-28 02:02:39 +0300

    eshop class update

--- SaveThem/app/src/main/java/com/example/savethem/eshop.java
+++ SaveThem/app/src/main/java/com/example/savethem/eshop.java
@@ -2,19 +2,14 @@ package com.example.savethem;
 
 public class eshop
 {
-<<<<<<< Updated upstream
-    private int productId;
-    private List<int> wishList = new ArrayList<int>; // product id will be added
-
-    public eshop(int productId,int[] wishlist)
-=======
-    private int[] productId = new int [20];;
-    private int[] wishlist=new int [20];
-    public eshop(int[] productId,int[] wishlist)
->>>>>>> Stashed changes
+    private int[] wishList = new int[20]; // product id will be added
+    private int[] productId = new int[20];
+    private int w_index=0;
+    private int p_index=0;
+    public eshop(int[] productId,int[] wishList)
     {
         this.productId=productId;
-        this.wishlist=wishlist;
+        this.wishList=wishList;
     }
     public void updateStock()
     {
@@ -22,17 +17,19 @@ public class eshop
         System.out.println("Stock is updated!");
     }
     //getters
-<<<<<<< Updated upstream
-    public int getProductId() { return productId; }
-    public List<int> getWishlist() { return wishList; }
-    //setters
-    public void setProductId(int productId) {this.productId=productId;}
-    public void addWishListItem(int productId) {this.wishlist.add(productId);}
-=======
     public int[] getProductId() { return productId; }
-    public int[] getWishlist() { return productId; }
+    public int[] getWishlist() { return wishList; }
     //setters
-    public void setProductId(int[] productId) {this.productId=productId;}
-    public void setWishlist(int[] wishlist) {this.wishlist=wishlist;}
->>>>>>> Stashed changes
+    public void addWishListItem(int product)
+    {
+        int i = w_index++;
+        productId[i]=product;
+    }
+    //setters
+    public void setProductId(int product)
+    {
+        int i = p_index++;
+        productId[i]=product;
+    }
+
 }
