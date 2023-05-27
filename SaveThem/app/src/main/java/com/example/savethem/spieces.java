d810284e6dacacd66b5299d41a75e404cafd3446
Theodore Giannak <59723723+TheodoreGiannak@users.noreply.github.com>
2023-05-27 16:59:20 +0300

    class spieces v1

---
+++ SaveThem/app/src/main/java/com/example/savethem/spieces.java
@@ -0,0 +1,26 @@
+package com.example.savethem;
+
+abstract class spieces
+{
+    protected int id;
+    protected int popularity;
+    protected String name;
+    protected String description;
+    protected String location;
+    protected String 
+
+    public spieces(int id, int popularity, String name, String description, String location)
+    {
+        this.id=id;
+        this.popularity=popularity;
+        this.name=name;
+        this.description=description;
+        this.location=location;
+    }
+
+    public static  
+
+}
+
+
+
