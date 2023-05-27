f5c43ea21cac61a9f466bea2286a54a083394ee3
Theodore Giannak <59723723+TheodoreGiannak@users.noreply.github.com>
2023-05-27 17:13:34 +0300

    Update spieces.java
    spieces class v2

--- SaveThem/app/src/main/java/com/example/savethem/spieces.java
+++ SaveThem/app/src/main/java/com/example/savethem/spieces.java
@@ -1,5 +1,5 @@
 package com.example.savethem;
-
+//this class should be replaces by database, but it is used instead
 abstract class spieces
 {
     protected int id;
@@ -7,18 +7,24 @@ abstract class spieces
     protected String name;
     protected String description;
     protected String location;
-    protected String 
+    protected String animalType;
 
-    public spieces(int id, int popularity, String name, String description, String location)
+    public spieces(int id, int popularity, String name, String description, String location, String animalType)
     {
         this.id=id;
         this.popularity=popularity;
         this.name=name;
         this.description=description;
         this.location=location;
+        this.animalType=animalType;
     }
 
-    public static  
+    public int getId() {return id;}
+    public int getPopularity() {return popularity;}
+    public String getName() {return name;}
+    public String getDescription() {return description;}
+    public String getLocation() {return location;}
+    public String getAnimalType() {return animalType;}
 
 }
 
