295db60e64ba2ea4d8d9200b7bf4af54a8223790
CPP-Bot-78 <kzachoulitis@gmail.com>
2023-05-27 21:41:26 +0300

    small changes to eshop class

--- README.md
+++ README.md
@@ -1,5 +1,12 @@
-# Save Them
-![logo](https://github.com/CPP-Bot-78/Software-Engineering/SaveThem/app/logo.jpg)
 
+# Save Them
+
+<center>
+<img src="https://raw.githubusercontent.com/CPP-Bot-78/Software-Engineering/main/SaveThem/app/logo.jpg" width="340" height="350">
+
+[https://raw.githubusercontent.com/CPP-Bot-78/Software-Engineering/main/SaveThem/app/logo.jpg)]: #
 ## A Software Engineering Project, 2023, CEID UPATRAS
+
+</center>
+
 Save them is an Andoid app designed to help it's users to learn about endangered species and what to do in order to protect them
--- SaveThem/app/src/main/java/com/example/savethem/ui/eshop/EshopFragment.java
+++ SaveThem/app/src/main/java/com/example/savethem/ui/eshop/EshopFragment.java
@@ -1,8 +1,39 @@
 package com.example.savethem.ui.eshop;
 
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.example.savethem.databinding.FragmentEshopBinding;
+import com.example.savethem.ui.eshop.ProductsPageViewModel;
 
 public class EshopFragment extends Fragment
 {
+    private FragmentEshopBinding binding;
 
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        ProductsPageViewModel productsPageViewModel =
+                new ViewModelProvider(this).get(ProductsPageViewModel.class);
+
+        binding = FragmentEshopBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
+
+        final TextView textView = binding.textHome;
+        productsPageViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        return root;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+}
 }
