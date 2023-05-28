27562ee47a9fc676ce5be399d255a85d728dd880
CPP-Bot-78 <kzachoulitis@gmail.com>
2023-05-28 22:48:01 +0300

    Update MKO_Authorities.java

--- SaveThem/app/src/main/java/com/example/savethem/MKO_Authorities.java
+++ SaveThem/app/src/main/java/com/example/savethem/MKO_Authorities.java
@@ -1,18 +1,25 @@
 package com.example.savethem;
 
-public class MKO_Authorities {
+public class MKO_Authorities
+{
+     private String MKO_Name;
+     private String Street;
+     private int Number;
+     private String City;
+     private String Country;
+     private int MKO_Code;
 
-private String MKO_Name;
-private String Street;
-private INT Number;
-private String City;
-private String Country;
-private INT MKO_Code;
-
-public void LoadMKO() {
-
-    
-     System.out.println("Content loaded.");    
-
+     public MKO_Authorities(String MKO_Name,String Street,int Number,String City,String Country,int MKO_Code)
+     {
+          this.MKO_Name=MKO_Name;
+          this.Street=Street;
+          this.Number=Number;
+          this.City=City;
+          this.Country=Country;
+          this.MKO_Code=MKO_Code;
      }
+     public void LoadMKO() {
+          System.out.println("Content loaded.");
+     }
+}
 
