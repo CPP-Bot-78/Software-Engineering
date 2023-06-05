6a9f97f760afa535bbd6c3cee78afdd9b33cf658
Mike <michalissoko@gmail.com>
2023-06-05 20:32:12 +0300

    MKO Donation

---
+++ SaveThem/app/src/main/java/com/example/savethem/MKO Donation.java
@@ -0,0 +1,137 @@
+package com.example.savethem;
+
+public class MKO_List {
+
+   private String MKO_Name;
+   private INT MKO_Code;
+   private INT[] MKO_List;
+
+
+   public show(String MKO_Name,INT MKO_Code,INT[] MKO_List;) {
+
+   this.MKO_Name=MKO_Name;
+   this.MKO_Code=MKO_Code;
+   this.MKO_List=MKO_List;
+    
+   }
+
+   public string getMKO_Name() {return MKO_Name;} 
+   public INT getMKO_Code() {return MKO_Code;}
+   public INT getMKO_List() {return MKO_List;}
+
+
+ public void setMKO_Name() {this.MKO_Name=MKO_Name;}
+ public void setMKO_Name() {this.MKO_Code=MKO_Code;}
+ public void setMKO_List() {this.MKO_List=MKO_List;}
+
+
+}  
+
+
+  public void selectM.K.O() {
+
+  System.out.println("Please select MKO.");
+
+  }
+
+
+  
+
+
+package com.example.savethem;
+
+public class Bank {
+
+  private String BankName;
+  private INT BankAccount;  
+
+  }
+
+
+  public void BankConfirmation() {
+
+  System.out.println("Please insert PIN.");
+  System.out.println("Please insert amount.");
+
+  {
+
+}
+
+
+
+
+package com.example.savethem;
+
+public class MobileDevice {
+
+
+  public void GPS_Permission() {
+
+  System.out.println("Your device needs location permission.");
+
+  {
+
+
+}
+
+
+
+
+package com.example.savethem;
+
+publc class NearestMKO_List {
+
+ private String MKO_Name;
+ private INT MKO_Code;
+ private INT[] MKO_List;
+
+
+ public show(String MKO_Name,INT MKO_Code,INT[] MKO_List;) {
+
+    this.MKO_Name=MKO_Name;
+    this.MKO_Code=MKO_Code;
+    this.MKO_List=MKO_List;
+    
+    }
+
+ public string getMKO_Name() {return MKO_Name;} 
+ public INT getMKO_Code() {return MKO_Code;}
+ public INT getMKO_List() {return MKO_List;}
+
+
+ public void setMKO_Name() {this.MKO_Name=MKO_Name;}
+ public void setMKO_Name() {this.MKO_Code=MKO_Code;}
+ public void setMKO_List() {this.MKO_List=MKO_List;}
+
+
+}
+
+
+
+
+package com.example.savethem;
+
+public class GPS_App {
+
+ public void OpenGPS_App() {
+ 
+  System.out.println("Please insert destination.");
+
+ }
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
