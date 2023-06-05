c4dba53292ef567a03fc4578df5958e4a244f4ab
Mike <michalissoko@gmail.com>
2023-06-05 20:32:48 +0300

    Injured Spiecies

---
+++ SaveThem/app/src/main/java/com/example/savethem/Injured Spieces.java
@@ -0,0 +1,28 @@
+package com.example.savethem;
+
+public class MobileDevice {
+
+
+  public void GPS_Permission() {
+
+  System.out.println("Your device needs location permission.");
+
+  {
+
+
+  public void MessageSendTo() {
+
+  System.out.println("A new message received.");
+
+  }
+
+  
+  public void GPS_Permission() {
+
+  System.out.println("Your device needs location permission.");
+
+  {
+ 
+    
+
+}
