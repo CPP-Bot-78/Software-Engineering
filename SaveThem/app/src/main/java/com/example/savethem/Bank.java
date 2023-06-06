2b580a7f3b5db1cd8bd1d331de9a6754957784bb
CPP-Bot-78 <kzachoulitis@gmail.com>
2023-06-06 10:41:27 +0300

    casual error fixing...

---
+++ SaveThem/app/src/main/java/com/example/savethem/Bank.java
@@ -0,0 +1,14 @@
+package com.example.savethem;
+
+public class Bank {
+
+    private String BankName;
+    private int BankAccount;
+
+    public void BankConfirmation()
+    {
+
+        System.out.println("Please insert PIN.");
+        System.out.println("Please insert amount.");
+    }
+}
---
+++ SaveThem/app/src/main/java/com/example/savethem/MobileDevice.java
@@ -0,0 +1,12 @@
+package com.example.savethem;
+
+public class MobileDevice {
+
+    public MobileDevice(){}
+    public void GPS_Permission() {
+
+        System.out.println("Your device needs location permission.");
+
+    }
+}
+
