9fa40a62c7b14b4c8e4106b8914b198d2395c0e9
Theodore Giannak <59723723+TheodoreGiannak@users.noreply.github.com>
2023-06-11 00:22:53 +0300

    spiecesList, animal found, species selection
    report animal found: ui choose camera or gallery
    
    speciesList: try to print a list of species class objects
    
    species_selection: window that chooses which animal did the user saw

--- SaveThem/app/src/main/java/com/example/savethem/speciesList.java
+++ SaveThem/app/src/main/java/com/example/savethem/speciesList.java
@@ -1,9 +1,35 @@
 package com.example.savethem;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
 import java.util.*;
 
-public class speciesList {
-    List<species> listOfSpecies=new ArrayList<species>();
+public class speciesList extends ArrayAdapter<species> {
+    private List<species> listOfSpecies;
+    public speciesList(Context context, List<species> listOfSpecies) {
+        super(context, 0, listOfSpecies);
+        this.listOfSpecies = listOfSpecies;
+    }
 
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        if (convertView == null) {
+            convertView = LayoutInflater.from(getContext()).inflate(R.layout.fragment_encuclopedia_items, parent, false);
+        }
+
+        species Species = listOfSpecies.get(position);
+
+        // Bind the data to the views in the item layout
+        // For example, TextView textView = convertView.findViewById(R.id.textView);
+        // textView.setText(myClass.getName());
+
+        return convertView;
+    }
     public void addListEntities(species x)
     {
         listOfSpecies.add(x);
